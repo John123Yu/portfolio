6bdc6096baeceb2dde5e91139b1a47e07900c5dc
Yu, John-NONEMP <jyu-NONEMP@Collegeboard.org>
2022-07-30 08:30:19 -0400

    portfolio comp

--- src/App.js
+++ src/App.js
@@ -1,19 +1,36 @@
-import React, { useState } from 'react';
-import './App.css';
-import Header from './components/Header';
-import Footer from './components/Footer';
+import React, { useState } from 'react'
+import styled from 'styled-components'
 
-const categories = ['About Me', 'Portfolio', 'Contact', 'Resume']
+import './App.css'
+import Header from './components/Header'
+import Footer from './components/Footer'
+import About from './components/About'
 
-function App() {
+const ABOUT_ME = 'About Me'
+const PORTFOLIO = 'Portfolio'
+const CONTACT = 'Contact'
+const RESUME = 'Resume'
+const categories = [ABOUT_ME, PORTFOLIO, CONTACT, RESUME]
+
+const StyledPage = styled.div`
+  display: flex;
+  justify-content: center;
+  padding: 20px 50px;
+`
+function App () {
   const [currentCategory, setCurrentCategory] = useState(categories[0])
 
   return (
     <div>
-      <Header categories={categories} currentCategory={currentCategory} setCurrentCategory={setCurrentCategory} />
+      <Header
+        categories={categories}
+        currentCategory={currentCategory}
+        setCurrentCategory={setCurrentCategory}
+      />
+      <StyledPage>{currentCategory === ABOUT_ME && <About />}</StyledPage>
       <Footer />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/components/About/index.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function About () {
+  return (
+    <>
+      <img height='200px' src={require('../../assets/images/avatar.png')}></img>
+      <section className='my-5'>
+        <p>
+          John is a Full Stack developer specializing in JavaScript and Python.
+          He also has an AWS solutions architect associate certificate.
+        </p>
+      </section>
+    </>
+  )
+}
+
+export default About

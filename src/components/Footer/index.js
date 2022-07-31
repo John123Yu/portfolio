7c8d5d531a0554b1e3af89fc8d12bd05b3c58315
Yu, John-NONEMP <jyu-NONEMP@Collegeboard.org>
2022-07-31 07:22:44 -0400

    gh-pages

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -2,9 +2,14 @@ import React from 'react'
 
 function Footer () {
   return (
-    <section className='my-5'>
-      <p><a href="https://github.com/john123yu" target="_blank" rel="noreferrer">Github</a></p>
-    </section>
+    <div style={{ padding: '20px 50px' }}>
+    <p>Links</p>
+      <p>
+        <a href='https://github.com/john123yu' target='_blank' rel='noreferrer'>
+          - Github
+        </a>
+      </p>
+    </div>
   )
 }
 
